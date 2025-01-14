09228ee300e3705463ccc344f4dc546fcfc621da
Tirumala Rao  S <tiru@live.in>
2025-01-14 17:27:39 +0530

    updated app

--- app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { FarmerQuestionnaireComponent } from './farmer-questionnaire/farmer-questionnaire.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'questionnaire', pathMatch: 'full' },
-  { path: 'questionnaire', component: FarmerQuestionnaireComponent },
-];
