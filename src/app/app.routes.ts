1efc187db693bc6e0dbf96355b66704bd6b7ce44
Tirumala Rao  S <tiru@live.in>
2025-01-16 18:06:49 +0530

    addedfarmerregistration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FarmerQuestionnaireComponent } from './farmer-questionnaire/farmer-questionnaire.component';
 import { BusinessQuestionnaireComponent } from './business-questionnaire/business-questionnaire.component';
 import { BusinessRegistrationComponent } from './business-registration/business-registration.component';
+import { FarmerRegistrationComponent } from './farmer-registration/farmer-registration.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'farmersurvey', pathMatch: 'full' },
   { path: 'farmersurvey', component: FarmerQuestionnaireComponent },
   { path: 'businesssurvey', component: BusinessQuestionnaireComponent },
   { path: 'businessregistration', component: BusinessRegistrationComponent },
+  { path: 'farmerregistration', component: FarmerRegistrationComponent },
 ];
