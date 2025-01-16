0cc6895090008646ae1aea3a2bf57e3340323b64
Tirumala Rao  S <tiru@live.in>
2025-01-16 09:54:10 +0530

    changed businessfeedback to businessquestionnaire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
 import { FarmerQuestionnaireComponent } from './farmer-questionnaire/farmer-questionnaire.component';
-import { BusinessFeedbackComponent } from './business-feedback/business-feedback.component';
+import { BusinessFeedbackComponent } from './business-questionnaire/business-questionnaire.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'questionnaire', pathMatch: 'full' },
   { path: 'questionnaire', component: FarmerQuestionnaireComponent },
-  { path: 'businessfeedback', component: BusinessFeedbackComponent },
+  { path: 'businessquestionnaire', component: BusinessFeedbackComponent },
 ];
